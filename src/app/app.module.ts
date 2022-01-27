70e93fee0256014f80c29a4b88230d07c10b372f
= <49078581+walase01@users.noreply.github.com>
2022-01-26 21:08:16 -0400

    addin code

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,21 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { SharedModule } from './shared/shared.module';
+import { AppRouterModule } from './app-router.module';
 
 import { AppComponent } from './app.component';
+
+
+
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRouterModule,
+    SharedModule
   ],
   providers: [],
   bootstrap: [AppComponent]
